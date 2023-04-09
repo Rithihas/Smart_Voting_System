aa8d0a9e2c811bcbb5993b17660922825920acdb
Rithihas <rithihasz@gmail.com>
2023-04-09 12:47:33 +0530

    face recognition implemented

--- face_recognition/validateFace.py
+++ face_recognition/validateFace.py
@@ -1,38 +1,93 @@
 #final module/code
 
 import sys
-# caution: path[0] is reserved for script path (or '' in REPL)
-sys.path.insert(1, 'D:/CVRnotes/year3sem2/mini project/Smart_Voting_System/face_recognition/Lib/site-packages')
+#  path[0] is reserved for script path
+# this is for providing location of our packages 
+sys.path.insert(1, 'D:/CVRnotes/year3sem2/mini project/Smart_Voting_System/face_recognition/virtual/Lib/site-packages')
 
-from pymongo import MongoClient
-import base64
-from deepface import DeepFace
-from flask import Flask
+from pymongo import MongoClient #for connecting to DB
+from PIL import Image # for converting base64 decoded image to bytes array
+import base64 # for base64 decoding
+import io 
+from deepface import DeepFace # for face-recognition models
+from flask import Flask, request, jsonify # for creating server
+import numpy as np  # for converting bytes array to numpy array
+from flask_cors import CORS # For security reasons, browsers restrict cross-origin HTTP requests initiated from scripts. For example, XMLHttpRequest and the Fetch API follow the same-origin policy. This means that a web application using those APIs can only request resources from the same origin the application was loaded from unless the response from other origins includes the right CORS headers.
+#this allows us to perform cross origin requests.
 
-app = Flask(__name__)
+app = Flask(__name__) #initializing instance of Flask object.
+cors = CORS(app) # I dont know what this is doing 
 
-@app.route('/facevalidate')
-def validateFace(username,password,cimagepath):
+@app.route('/validate', methods = ["POST"]) # url to call this method
+def validateFace():
+
+    data = request.get_json()  #extracting request data
+    # data = jsonify(data)
+
+    
+    #connecting to mongo DB databse        
+    client = MongoClient("mongodb+srv://rithihas:votingsmart@smartvoting.xgicig5.mongodb.net/?retryWrites=true&w=majority")
+ 
+    dbname = client["smartvoting"] 
+
+    cname = dbname["voters"] #getting required collection
+
+    # print(data)
+    
+    retrieved = cname.find_one({"username" : data[0]["user"]}) #retrieving record from collection
+     
+    decode = base64.b64decode(retrieved['photo'])  # decoding base64 string retrieved from database
+
+    # converting the decode object into numpy array (deepface is compatible with numpy arrays)
+    image = Image.open(io.BytesIO(decode))
+    dbim = np.array(image)
+
+    # doing the same with webcam photo
+    cdecode = base64.b64decode(data[0]["photo"])
+
+    currentimage = Image.open(io.BytesIO(cdecode))
+    cim = np.array(currentimage)
+
+      
+    # verifying using deepface (model used: VGG)
+    result = DeepFace.verify(dbim,cim,enforce_detection=False)
+    
+    #comparing the dissimilarity between the faces , and returning result
+    if result['distance'] < 0.32:
+        return {"fresult" : "true"}
+    else:
+        return {"fresult" : "false"}
+    
+
+
+#for inserting records into the database
+
+@app.route('/insert', methods = ["POST"])
+def insertFace():
+
+    data = request.get_json()
+    # data = jsonify(data)
+
+    
             
     client = MongoClient("mongodb+srv://rithihas:votingsmart@smartvoting.xgicig5.mongodb.net/?retryWrites=true&w=majority")
  
     dbname = client["smartvoting"]
 
     cname = dbname["voters"]
+
+    photo_string = data[0]['photo']
+
+    user = data[0]['user']
+
+    password = data[0]['pass']
+
+    voter = {"username" : user , "password" : password , "photo"  : photo_string}
+
+
+    inserted = cname.insert_one(voter)
+
+    return {"fresult" : "inserted"}
+
     
-    retrieved = cname.find_one({"username" : username})
-    
-    img = base64.b64decode((retrieved['photo']))
-    with open("D:/CVRnotes/year3sem2/mini project/Smart_Voting_System/face_recognition/image_staging/dbimage.jpg", "wb") as fh:
-        fh.write(img)
-        
-    
-    result = DeepFace.verify(img1_path = "D:/CVRnotes/year3sem2/mini project/Smart_Voting_System/face_recognition/image_staging/dbimage.jpg" , img2_path =cimagepath,enforce_detection=False)
-
-
-
-
-    if result['distance'] < 0.32:
-        return 1
-    else:
-        return 0
+  
