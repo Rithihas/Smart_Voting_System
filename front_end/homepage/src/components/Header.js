3a012ecb5ade1991ac9e70309b31cf2a7b90baf5
REDDEBOINASOUJANYA <119043240+reddeboinasoujanya09@users.noreply.github.com>
2023-05-12 12:07:17 +0530

    Add files via upload
    header updated

--- front_end/homepage/src/components/Header.js
+++ front_end/homepage/src/components/Header.js
@@ -8,8 +8,8 @@ export default function Header(){
         <button id="open" className="nav-button">&#9776;</button>
         <nav className="nav" aria-label="main">
         <Link to="/" className="anchor">Home</Link>
-            <Link to="/register" className="anchor">Register</Link> 
-            <Link to="/login" className="anchor">Login</Link>
+            <a href="reg.html" className="anchor">Register</a> 
+            <a href="login.html" className="anchor">Login</a>
             <Link to="/result" className="anchor">Results</Link>
             </nav>
     </div>
