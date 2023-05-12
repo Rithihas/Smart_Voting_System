9e5c2079bf40313ccb5da35416aa5ddfc9022f77
reddeboinasoujanya09 <reddeboinasoujanya@gmail.com>
2023-05-12 09:45:33 +0530

    home page implemented partially

--- front_end/homepage/src/App.js
+++ front_end/homepage/src/App.js
@@ -1,9 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
-import Home from './components/home'
+import Home from './components/Home';
+
+import Result from './components/Results';
+
+import Header from './components/Header';
+import { Route,Routes } from 'react-router-dom';
 function App() {
   return (
-    <Home/>
+    <div>
+      <Header/>
+      <Routes>
+        <Route path='/' element={<Home/>}></Route>
+        
+       
+        <Route path='/result' element={<Result/>}></Route>
+        </Routes>
+    </div>
    
   );
 }
